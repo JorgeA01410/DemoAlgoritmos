750ef549505c63955bb32857796ace59c040d521
JorgeAvila0924 <142038848+JorgeA01410@users.noreply.github.com>
2025-10-02 18:28:42 -0600

    Primer comit

---
+++ Prueba/Entidades/Planta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.Entidades
+{
+    public class Planta
+    {
+        
+        public string IdPlanta { get; set; }
+        public string IdSolucion { get; set; }
+        public int IdLote { get; set; }
+
+        public Planta(Planta planta)
+        {
+            IdPlanta = planta.IdPlanta;
+            IdSolucion = planta.IdSolucion;
+            IdLote = planta.IdLote;
+        }
+
+    }
+}
