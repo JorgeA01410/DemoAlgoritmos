0bd2c0305cea1a8cc6fd5687f730cb54f05b59d2
JorgeAvila0924 <142038848+JorgeA01410@users.noreply.github.com>
2025-10-02 18:44:14 -0600

    Cambios de interfaz

--- Prueba/Form1.Designer.cs
+++ Prueba/Form1.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 19F);
+            button1.Location = new Point(414, 150);
+            button1.Name = "button1";
+            button1.Size = new Size(300, 48);
+            button1.TabIndex = 0;
+            button1.Text = "EJECUTAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(414, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(345, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19F);
+            label1.Location = new Point(183, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(185, 36);
+            label1.TabIndex = 2;
+            label1.Text = "PARAMETRO 1";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(183, 250);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(692, 328);
+            dataGridView1.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1139, 763);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private DataGridView dataGridView1;
     }
 }
